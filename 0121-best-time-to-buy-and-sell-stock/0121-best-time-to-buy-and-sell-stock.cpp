7a1ae10a2123c344a1c5f450905f80cb26aef7a4
debarkac <115308374+debarkac@users.noreply.github.com>
2023-10-16 22:00:33 +0530

    Time: 100 ms (35.94%), Space: 93.8 MB (16.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    //brute force approach
-    //     int maxP=0;
-    //    for(int i=0;i<prices.size();i++){
-    //        for(int j=i+1;j<prices.size();j++){
-    //            if(prices[j]-prices[i]>maxP)
-    //                 maxP=prices[j]-prices[i];
-    //        }
-    //    }
-    //    return maxP;
-
-
-
-    int maxP=0,min=INT_MAX;
-    for(int i=0;i<prices.size();i++){
-        if(prices[i]>min && prices[i]-min>maxP)
-            maxP=prices[i]-min;
-        else if(prices[i]<min)
-            min=prices[i];
-    }
-    return maxP;
+        int maxP=0,min=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]>min && prices[i]-min>maxP)
+                maxP=prices[i]-min;
+            else if(min>prices[i])
+                min=prices[i];
+        }
+        return maxP;
     }
 };

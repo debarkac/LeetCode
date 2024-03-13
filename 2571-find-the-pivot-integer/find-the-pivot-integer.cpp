488f1f98c1565d64d7da41b0360704230a7a7d26
Debarka Chakraborti <115308374+debarkac@users.noreply.github.com>
2024-03-13 22:49:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (15.68%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        double x=sqrt(n*(n+1)/2);
+        if(fmod(x,1.00)!=0)
+            return -1;
+        else
+            return (int)sqrt(n*(n+1)/2);
+    }
+};

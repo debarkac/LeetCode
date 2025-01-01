1d142965607a064c69821fcdc6d4195240cf3195
Debarka Chakraborti <115308374+debarkac@users.noreply.github.com>
2025-01-01 23:07:48 +0530

    Time: 58 ms (5.34%) | Memory: 64 MB (5.20%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int check(vector<int> left){
+        int c=0;
+
+        
+        for(int i=0;i<left.size();i++){
+            if(left[i]==0)
+                c++;
+        }
+        return c;
+    }
+
+    int check_ones(vector<int> right){
+        int c=0;
+
+        
+        for(int i=0;i<right.size();i++){
+            if(right[i]==1)
+                c++;
+        }
+        return c;
+    }
+
+    int maxScore(string s) {
+        vector<int> arr;
+        for(int i=0;i<s.length();i++){
+
+            char digit=s[i]-'0';
+            arr.push_back(digit);
+        }
+        // if(arr.size()==2)
+        //     return 1;
+        int score=0;
+        int k=1;
+        for(int i=0;i<arr.size()-1;i++){
+            vector<int> left;
+            vector<int> right;
+            for(int j=0;j<k;j++){
+                left.push_back(arr[j]);
+            }
+            for(int j=k;j<arr.size();j++){
+                right.push_back(arr[j]);
+            }
+            k++;
+            int l=check(left);
+            int r=check_ones(right);
+            score=(l+r)>score?(l+r):score;
+        }
+        return score;
+    }
+};

a3e7734ecd6f89f6475db7a2ce83d1c59b8afb7f
Debarka Chakraborti <115308374+debarkac@users.noreply.github.com>
2025-09-24 23:28:16 +0530

    Time: 4 ms (86.97%) | Memory: 57 MB (90.76%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -10,9 +10,9 @@ class Solution {
         }
 
         for(int i=0;i<nums.length;i++){
-            right_sum=total_sum-nums[i]-left_sum;//5
-            int left_size=i;//1
-            int right_size=nums.length-i-1;//1
+            right_sum=total_sum-nums[i]-left_sum;
+            int left_size=i;
+            int right_size=nums.length-i-1;
 
             res[i]=(left_size*nums[i]-left_sum)+(right_sum-right_size*nums[i]);
             left_sum=left_sum+nums[i];
